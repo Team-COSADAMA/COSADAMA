00a1c063c707448c818b6d8c4a911c2bc451ee7d
Peniel Cho <penielcho98@aol.com>
2022-02-02 01:11:31 +0900

    og image

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,53 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: '반갑습니다. 중앙대학교 비전공자 코딩 커뮤니티 코사다마입니다.' },
+      { name: 'format-detection', content: 'telephone=no' },
+      // Twitter
+      // Test on: https://cards-dev.twitter.com/validator
+      {
+        hid: 't-type',
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      // Open Graph
+      // Test on: https://developers.facebook.com/tools/debug/
+      { hid: 'og:site_name', property: 'og:site_name', content: 'COSADAMA' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://www.cosadama.com'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'COSADAMA'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          '반갑습니다. 중앙대학교 비전공자 코딩 커뮤니티 코사다마입니다.'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://raw.githubusercontent.com/Team-COSADAMA/COSADAMA/main/static/og_image.png'
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: 'https://raw.githubusercontent.com/Team-COSADAMA/COSADAMA/main/static/og_image.png'
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'COSADAMA Main Image'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
     ]
   },
 
