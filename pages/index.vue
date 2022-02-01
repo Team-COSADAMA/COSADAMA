5a4aab4302d74ea6cb63c7497bfbf2f45f6402c4
Peniel Cho <penielcho98@aol.com>
2022-02-02 02:58:15 +0900

    temp README

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <div class="text-white text-4xl md:text-6xl pt-40 md:pt-72 mb-8 font-medium drop-shadow-md">Welcome to the Community, <br class="hidden sm:inline md:inline" /> Where We All Can Code.</div>
       <div class="text-slate-300 text-base md:text-xl md:text-2xl mb-20 md:mb-32 drop-shadow">코사다마는 중앙대학교의 <br class="inline md:hidden" /> 비전공자 코딩 커뮤니티입니다.</div>
 
-      <div class="text-3xl md:text-4xl text-white mb-5 md:mb-7 font-medium">Recruiting Now</div>
+      <div class="text-3xl md:text-4xl text-white mb-5 md:mb-7 font-medium">Now Recruiting</div>
       <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-8 mb-12 md:mb-24">
         <div class="rounded-lg h-80 md:h-96 section bg-img group" style="background-image: url(team1.jpg)">
           <div class="rounded-lg w-full h-full bg-gradient-to-t from-gray-700">
